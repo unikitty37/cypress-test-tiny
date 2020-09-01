c91a334bc7a9df1a0c970dc52a85a534db1528b4
John Y <unikitty37@users.noreply.github.com>
2020-09-01 11:54:07 +0100

    Test for stubbing images from fixtures

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.server()
+    cy.route('GET', `https://fillmurray.com/*`, 'fixture:image.jpg')
+    
+    cy.visit('https://unikitty37.github.io/cypress-test-tiny/')
   })
 })
